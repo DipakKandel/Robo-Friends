1d50cdf57295406ec79082c326eed0369e7588db
DipakKandel <Dipak.itsme@gmai.com>
2019-08-12 09:17:08 +0545

    just learning

---
+++ robofriends/src/Hello.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+ 
+class Hello extends Component {
+	render(){
+		return(
+		
+			<h1> Hello World </h1>
+			<p>My name is Dipak </p>
+		
+		
+		);
+
+	}
+
+}
+export default Hello ;
