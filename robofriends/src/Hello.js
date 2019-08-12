21e8685765932864753d344fde8736eb6bf762ef
DipakKandel <Dipak.itsme@gmai.com>
2019-08-12 09:29:17 +0545

    Deleted Unnecessary Files

--- robofriends/src/Hello.js
+++ robofriends/src/Hello.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
- 
+import './hello.css' 
 class Hello extends Component {
 	render(){
 		return(
-		
+		<>
 			<h1> Hello World </h1>
 			<p>My name is Dipak </p>
 		
-		
+		</>
 		);
 
 	}
